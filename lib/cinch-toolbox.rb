4fa38d969a93147f53b536cebd88bc307b9c13a5
Brian Haberer <bhaberer@gmail.com>
2013-05-19 13:49:26 -0700

    Adding a small alteration to OpenURI that allows for redirection between protocols.

--- lib/cinch-toolbox.rb
+++ lib/cinch-toolbox.rb
@@ -95,3 +95,10 @@ module Cinch
 
   end
 end
+
+module OpenURI
+  def OpenURI.redirectable?(uri1, uri2) # :nodoc:
+    uri1.scheme.downcase == uri2.scheme.downcase ||
+    (/\A(?:http|ftp)\z/i =~ uri1.scheme && /\A(?:http|ftp|https)\z/i =~ uri2.scheme)
+  end
+end
